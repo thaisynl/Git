e3e287b36d573d9147561f3bd793ad379b93d3f4
André Portela <portela.eng@gmail.com>
2021-06-09 22:08:11 -0300

    adicionada funcao de multiplicacao

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
+def mul(first_arg, second_arg):
+    result = first_arg * second_arg
+    print(f"mul({first_arg}, {second_arg}) = {result}")
+    return result
+
 def add(first_arg, second_arg):
     result = first_arg + second_arg
     print(f"add({first_arg}, {second_arg}) = {result}")
