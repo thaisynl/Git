088265284e85858b21ea4e5e9d769b4d6a7c5bb6
André Portela <portela.eng@gmail.com>
2021-06-09 22:06:18 -0300

    adicionado funcao de subtracao

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@ def add(first_arg, second_arg):
     result = first_arg + second_arg
     print(f"add({first_arg}, {second_arg}) = {result}")
     return result
+
+def sub(first_arg, second_arg):
+    result = first_arg - second_arg
+    print(f"sub({first_arg}, {second_arg}) = {result}")
+    return result
